cb6c7a2691a972133f758419aeee341991b10384
HypeByte <HypeByteProgrammer@gmail.com>
2022-06-18 00:53:46 -0700

    Added player ship and player movement

--- game/game.py
+++ game/game.py
@@ -16,15 +16,37 @@ player = pygame.image.load("./images/spaceship.png")
 player = pygame.transform.scale(player, (100, 100))
 playerX = 450
 playerY = 600
+playerSpeed = 0
 def spawnPlayer(x,y):
     game.blit(player, (x,y) )
 
 
 #game loop
 while rungame:
+    game.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            rungame = False 
-
+            rungame = False
+        
+        #Add x-axis movement
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                #Move left
+                playerSpeed = -0.7
+            if event.key == pygame.K_RIGHT:
+                #Move right
+                playerSpeed = 0.7
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                #Don't move
+                playerSpeed = 0
+    #Modify x-position of player based on their input
+    playerX+= playerSpeed
+    #Adds boundaries to the player ship so they can't go out of game window
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 900:
+        playerX = 900
+ 
     spawnPlayer(playerX, playerY)
     pygame.display.update()
