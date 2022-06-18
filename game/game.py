ecb400fd707f5f064802997fef67f089103f78d5
HypeByte <HypeByteProgrammer@gmail.com>
2022-06-18 00:06:56 -0700

    Programmed game gui and added assests

--- game/game.py
+++ game/game.py
@@ -1,2 +1,28 @@
 import pygame 
 import sys 
+
+#Start game
+pygame.init()
+
+#Make game window called game
+game = pygame.display.set_mode((1000,800))
+pygame.display.set_caption("Odyssey")
+icon = pygame.image.load(".\images\gameicon.png")
+pygame.display.set_icon(icon)
+rungame = True
+
+#Initialize player
+player = pygame.image.load("./images/spaceship.png")
+player = pygame.transform.scale(player, (100, 100))
+def spawnPlayer(x,y):
+    game.blit(player, (x,y) )
+
+
+#game loop
+while rungame:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            rungame = False 
+
+    spawnPlayer(450, 600)
+    pygame.display.update()
