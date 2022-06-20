3e7f023a6ffc7f2a167fe1c1c2387ed8a981719e
HypeByte <HypeByteProgrammer@gmail.com>
2022-06-19 22:33:10 -0700

    Testing my player object

--- game/game.py
+++ game/game.py
@@ -1,6 +1,7 @@
 import pygame 
 import random 
 import sys
+from util import player
 from util import element
 
 
@@ -15,7 +16,7 @@ pygame.display.set_icon(icon)
 rungame = True
 
 #Make game background
-background = element("./images/background.jpg", (1000, 800), (0,0), 0)
+background = element("./images/background.jpg", (1000, 800), (0,0))
 
 #Initialize player
 '''
@@ -27,12 +28,12 @@ playerSpeed = 0
 def spawnPlayer(x,y):
     game.blit(player, (x,y) )
 '''
-player = element("./images/spaceship.png", (75,75), [450, 675], 1)
+Player = player("./images/spaceship.png", (75,75), [450, 675], 1)
 
 #Initialize alien
 alienX = random.randint(0, 925)
 alienY = random.randint(0,350)
-alien = element("./images/alien.png", (75, 75), (alienX, alienY), 0)
+alien = element("./images/alien.png", (75, 75), (alienX, alienY))
 
 #game loop
 while rungame:
@@ -41,28 +42,10 @@ while rungame:
         if event.type == pygame.QUIT:
             rungame = False
         
-        #Add x-axis movement
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                #Move left
-                player.move(True)
-            if event.key == pygame.K_RIGHT:
-                #Move right
-                player.move(False)
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                pass
-                #Don't move
-                #player.velocity = 0
-    #Modify x-position of player based on their input
-    #Adds boundaries to the player ship so they can't go out of game window
-    '''
-    if playerX <= 0:
-        playerX = 0
-    elif playerX >= 925:
-        playerX = 925
-    '''
+        Player.xmove(event)
+        
+    
  
-    player.spawn(game)
+    Player.spawn(game)
     alien.spawn(game)
     pygame.display.update()
--- game/util.py
+++ game/util.py
@@ -4,17 +4,47 @@ import sys
 
 class element:
 
-    def __init__(self, sprite, scale, coords, velocity):
+    def __init__(self, sprite, scale, coords):
         self.sprite = pygame.transform.smoothscale((pygame.image.load(sprite)), scale)
         self.scale = scale
         self.coords = coords
-        self.velocity = velocity
         
 
     def spawn(self, screen):
         screen.blit(self.sprite, self.coords)
 
-    def move(self, direct):
-        self.coords[0]+= -(self.velocity) if direct == True else self.velocity
+
+
+class player(element):
+
+    def __init__(self, sprite, scale, coords, velocity):
+        self.sprite = pygame.transform.smoothscale((pygame.image.load(sprite)), scale)
+        self.scale = scale
+        self.coords = coords
+        self.velocity = velocity 
+
+    def xmove(self, event_handler):
+        if event_handler.type == pygame.KEYDOWN:
+
+            if event_handler.key == pygame.K_LEFT:
+                #Move left
+                self.coords[0]+= self.velocity
+
+            if event_handler.key == pygame.K_RIGHT:
+                #Move right
+                self.coords[0]+= self.velocity
+        
+        if event_handler.type == pygame.KEYUP:
+
+            if event_handler.key == pygame.K_LEFT or event_handler.key == pygame.K_RIGHT:
+                self.coords[0]+= 0
+    
+        if self.coords[0] <= 0:
+             self.coords[0] = 0
+
+        elif self.coords[0] >= 925:
+             self.coords[0] = 925
+                 
+
         
 
